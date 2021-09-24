6f37a1cb4f3ab5564b301845b12e54554d6ce0c3
lizardqueen98 <44054497+lizardqueen98@users.noreply.github.com>
2021-09-24 15:44:46 +0200

    Add files via upload

---
+++ installPackages.R
@@ -0,0 +1,49 @@
+install.packages('magrittr') # pipe operator
+install.packages('dplyr') # select, filter, etc.. operations on dataframes
+install.packages('tidyr') # helps making our data tidy with gather, spread and other functions
+install.packages('ggplot2') # plotting library, grammar of graphics
+install.packages('lubridate') # enables easy working with dates
+install.packages('readxl') # read xls and xlsx files
+install.packages('prettydoc') # makes r notebooks prettier
+install.packages('corrplot') # library for correlation plots
+install.packages('tidyverse') # includes multiple packages for cleaning and working with tidy data
+ # ggplot2
+ # dplyr
+ # tidyr
+ # readr
+ # purrr
+ # tibble
+ # stringr
+ # forcats
+install.packages('dslabs') # includes multiple interesting datasets
+install.packages('ggthemes') # makes ggplots nicer with nice themes
+install.packages('ggrepel') # nice geom_text placing on ggplots
+install.packages('gridExtra') # arrange multiple ggplots in one plot (grid of plots)
+install.packages('caret') # powerful machine learning package that helps you preprocess, train, test and validate your machine learning models
+
+#### 
+#### multiple packages for specific machine learning models
+####
+
+install.packages('e1071') 
+install.packages('elasticnet')
+install.packages('onehot')
+install.packages('xgboost')
+install.packages('LiblineaR')
+install.packages('randomForest')
+install.packages('kernlab')
+
+####
+#### for graphs
+####
+
+install.packages('devtools')
+install.packages('plotly')
+install.packages('gapminder')
+
+####
+#### for pdf
+####
+
+install.packages("webshot")
+webshot::install_phantomjs()
